fa8ca9d3406c101a189cfedc8c8bdd6809bd9c1f
devbest555 <martinbenka26@gmail.com>
2024-02-06 11:16:00 +0800

    change - change ownable error text in order to deploy everything again

--- test/main.test.js
+++ test/main.test.js
@@ -10,5 +10,5 @@
 // require('./research');
 // require('./multiSigWallet.test');
 // require('./gnosisSafe.test');
-require('./set-final-film.test');
-// require('./change-film-owner.test')
+// require('./set-final-film.test');
+require('./change-film-owner.test')
