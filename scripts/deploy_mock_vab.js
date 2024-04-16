94819cd48aafdb703d0247b62464f8d868a81a85
devbest555 <martinbenka26@gmail.com>
2024-04-16 10:39:43 +0800

    change - move deploy mock tokens

--- scripts/deploy_mock_vab.js
+++ scripts/deploy_mock_vab.js
@@ -1,15 +1,28 @@
-module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
+module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId, run }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy('MockERC20', {
+  const contract = await deploy('MockERC20', {
     from: deployer,
     args: ['Vabble', 'VAB'],
     log: true,
     deterministicDeployment: false,
     skipIfAlreadyDeployed: true,
   });
+
+  try {
+    await run("verify:verify", {
+        address: contract.address,
+        constructorArguments: ['USDT', 'USDT'],
+    })
+  } catch (e) {
+      if (e.message.toLowerCase().includes("already verified")) {
+          console.log("Already verified!")
+      } else {
+          console.log(e)
+      }
+  }
 };
 
 module.exports.id = 'deploy_vab'
-module.exports.tags = ['MockERC20'];
+module.exports.tags = ['MockERC20', 'Deploy'];
