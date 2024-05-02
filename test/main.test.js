5b4d356c2a531683733293c277e8612562dadb68
devbest555 <martinbenka26@gmail.com>
2024-05-03 00:33:53 +0800

    change - change test case for amoy and base testnet.

--- test/main.test.js
+++ test/main.test.js
@@ -3,13 +3,14 @@
 // require('./owner.test');
 // require('./vote.test');
 // require('./vabbleDAO.test');
-// require('./stakingPool.test');
+require('./stakingPool.test');
+// require('./stakerMap.test');
 // require('./factoryFilmNFT.test');
 // require('./factorySubNFT.test');
 // require('./subscription.test');
 // require('./research');
 // require('./multiSigWallet.test');
 // require('./gnosisSafe.test');
-require('./set-final-film.test');
+// require('./set-final-film.test');
 // require('./change-film-owner.test')
 // require('./unihelper.test');
