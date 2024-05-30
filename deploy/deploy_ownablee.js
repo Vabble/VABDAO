2b3b7902bef9942c148cab782d46ce4afbb42fe4
Mud <44410798+MudDev@users.noreply.github.com>
2024-05-30 06:10:01 -0600

    Fix for GnosisSafeL2

--- deploy/deploy_ownablee.js
+++ deploy/deploy_ownablee.js
@@ -1,39 +1,39 @@
 module.exports = async function ({ getNamedAccounts, deployments }) {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+	const { deploy } = deployments;
+	const { deployer } = await getNamedAccounts();
 
-  const { getConfig } = require('../scripts/utils');
+	const { getConfig } = require('../scripts/utils');
 
-  const network = await ethers.provider.getNetwork();
-  const chainId = network.chainId;
+	const network = await ethers.provider.getNetwork();
+	const chainId = network.chainId;
 
-  let GnosisSafeAddress = GnosisSafeL2;
-  if (GnosisSafeAddress == "") {
-    this.GnosisSafe = await deployments.get('GnosisSafeL2');  
-    GnosisSafeAddress = this.GnosisSafe.address;
-  }
+	const { vabToken, usdcAdress, walletAddress, GnosisSafeL2 } = getConfig(chainId);
 
-  const {vabToken, usdcAdress, walletAddress, GnosisSafeL2} = getConfig(chainId);
+	let GnosisSafeAddress = GnosisSafeL2;
+	if (GnosisSafeAddress == "") {
+		this.GnosisSafe = await deployments.get('GnosisSafeL2');
+		GnosisSafeAddress = this.GnosisSafe.address;
+	}
 
-  console.log("------------- Ownablee Deployment -----------------");
-  console.log({vabToken, usdcAdress, walletAddress, GnosisSafeL2});
-    
-  await deploy('Ownablee', {
-    from: deployer,
-    args: [
-      walletAddress,
-      vabToken, 
-      usdcAdress,
-      GnosisSafeAddress,
-      // "0xe0536a4D730a78DB8B4c4605D73e107201d9543e"
-      // "0x3E5e853d1784cDB519DB1eB175B374FB53FE285C"
-    ],
-    log: true,
-    deterministicDeployment: false,
-    skipIfAlreadyDeployed: false,
-  });
+	console.log("------------- Ownablee Deployment -----------------");
+	console.log({ vabToken, usdcAdress, walletAddress, GnosisSafeL2 });
 
-  this.Ownablee = await deployments.get('Ownablee');
+	await deploy('Ownablee', {
+		from: deployer,
+		args: [
+			walletAddress,
+			vabToken,
+			usdcAdress,
+			GnosisSafeAddress,
+			// "0xe0536a4D730a78DB8B4c4605D73e107201d9543e"
+			// "0x3E5e853d1784cDB519DB1eB175B374FB53FE285C"
+		],
+		log: true,
+		deterministicDeployment: false,
+		skipIfAlreadyDeployed: false,
+	});
+
+	this.Ownablee = await deployments.get('Ownablee');
 };
 
 module.exports.id = 'deploy_ownablee'
