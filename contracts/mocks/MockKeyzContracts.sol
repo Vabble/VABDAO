6db0bf67712e0b7c4d97dc30392fe56f5ed79a04
Mud <44410798+MudDev@users.noreply.github.com>
2024-11-10 19:03:27 -0700

    Refactor Mock contracts to use consistent naming for VAB token and WETH address, and streamline ETH handling

--- contracts/mocks/MockKeyzContracts.sol
+++ contracts/mocks/MockKeyzContracts.sol
@@ -4,45 +4,41 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ETHReceiver {
+    fallback() external payable {}
+
     receive() external payable {}
 }
 
 contract MockVabbleToken is ERC20 {
-    address payable public ethReceiver;
-
-    constructor(address payable _ethReceiver) ERC20("Vabble", "VAB") {
+    constructor() ERC20("Vabble", "VAB") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
-        ethReceiver = _ethReceiver;
     }
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
-
-    receive() external payable {
-        (bool success, ) = ethReceiver.call{value: msg.value}("");
-        require(success, "ETH transfer failed");
-    }
 }
 
 contract MockUniswapRouter {
-    address public immutable WETH;
-    address payable public immutable vabbleToken;
+    address public immutable WETH_ADDRESS; // Renamed from WETH
+    address public immutable vabToken;
 
-    event PathCheck(address[] path);
-    event Debug(string message, address addr1, address addr2);
-
-    constructor(address payable _vabbleToken, address _weth) {
-        require(_vabbleToken != address(0), "Invalid vabble token");
+    constructor(address _vabToken, address _weth) {
+        require(_vabToken != address(0), "Invalid VAB token");
         require(_weth != address(0), "Invalid WETH");
-        vabbleToken = _vabbleToken;
-        WETH = _weth;
+        vabToken = _vabToken;
+        WETH_ADDRESS = _weth;
+    }
+
+    // This function name stays the same as it's part of the interface
+    function WETH() external view returns (address) {
+        return WETH_ADDRESS;
     }
 
     function getAmountsOut(uint256 amountIn, address[] calldata path) external view returns (uint256[] memory) {
         require(path.length == 2, "Invalid path length");
-        require(path[0] == WETH, "First token must be WETH");
-        require(path[1] == address(vabbleToken), "Second token must be VAB");
+        require(path[0] == WETH_ADDRESS, "First token must be WETH");
+        require(path[1] == vabToken, "Second token must be VAB");
 
         uint256[] memory amounts = new uint256[](2);
         amounts[0] = amountIn;
@@ -55,21 +51,21 @@ contract MockUniswapRouter {
         address[] calldata path,
         address to,
         uint256 deadline
-    ) external payable returns (uint256[] memory amounts) {
+    ) external payable returns (uint256[] memory) {
         require(msg.value > 0, "Must send ETH");
         require(deadline >= block.timestamp, "Expired");
         require(path.length == 2, "Invalid path length");
-        require(path[0] == WETH, "First token must be WETH");
-        require(path[1] == address(vabbleToken), "Second token must be VAB");
+        require(path[0] == WETH_ADDRESS, "First token must be WETH");
+        require(path[1] == vabToken, "Second token must be VAB token");
 
-        uint256 amountOut = msg.value * 2;
-        require(amountOut >= amountOutMin, "Insufficient output amount");
-
-        MockVabbleToken(vabbleToken).mint(to, amountOut);
-
-        amounts = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
         amounts[0] = msg.value;
-        amounts[1] = amountOut;
+        amounts[1] = msg.value * 2; // 2x conversion rate
+
+        require(amounts[1] >= amountOutMin, "Insufficient output amount");
+
+        // Mint VAB tokens to recipient
+        MockVabbleToken(vabToken).mint(to, amounts[1]);
 
         return amounts;
     }
@@ -78,37 +74,39 @@ contract MockUniswapRouter {
 }
 
 contract MockStakingPool {
-    IERC20 public immutable vabbleToken;
+    IERC20 public immutable vabToken;
 
-    constructor(address _vabbleToken) {
-        require(_vabbleToken != address(0), "Invalid vabble token");
-        vabbleToken = IERC20(_vabbleToken);
+    constructor(address _vabToken) {
+        require(_vabToken != address(0), "Invalid VAB token");
+        vabToken = IERC20(_vabToken);
     }
 
     function addRewardToPool(uint256 amount) external {
-        require(vabbleToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(vabToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
     }
 }
 
 contract MockUniHelper {
-    address public immutable WETH;
-    address payable public immutable vabbleToken;
+    address public immutable WETH_ADDRESS;
+    address public immutable vabToken;
 
-    constructor(address payable _vabbleToken, address _weth) {
-        require(_vabbleToken != address(0), "Invalid vabble token");
+    constructor(address _vabToken, address _weth) {
+        require(_vabToken != address(0), "Invalid VAB token");
         require(_weth != address(0), "Invalid WETH");
-        WETH = _weth;
-        vabbleToken = _vabbleToken;
+        WETH_ADDRESS = _weth;
+        vabToken = _vabToken;
     }
 
     function getAmountsOut(uint256 amountIn, address[] calldata path) external view returns (uint256[] memory) {
         require(path.length == 2, "Invalid path length");
-        require(path[0] == WETH, "First token must be WETH");
-        require(path[1] == address(vabbleToken), "Second token must be VAB");
+        require(path[0] == WETH_ADDRESS, "First token must be WETH");
+        require(path[1] == vabToken, "Second token must be VAB");
 
         uint256[] memory amounts = new uint256[](2);
         amounts[0] = amountIn;
         amounts[1] = amountIn * 2;
         return amounts;
     }
+
+    receive() external payable {}
 }
