39d53ee197f5328f65de1c658ac387b854504510
Mill1995 <yannickBusinessMail@protonmail.com>
2024-12-11 16:50:53 +0100

    removed IERC20Metadata import: not used

--- contracts/dao/Property.sol
+++ contracts/dao/Property.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../libraries/Helper.sol";
 import "../interfaces/IUniHelper.sol";
